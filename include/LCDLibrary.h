654c5481bb5216babf70a17be507c2e0ca7e1566
WithoutAName25 <withoutaname@withoutaname.eu>
2023-12-30 22:19:57 +0100

    Add new header file LCDLibrary.h and update CMakeLists.txt
    Added a new header file, LCDLibrary.h, which makes inclusion of LCD functionality easier by wrapping it all into a single header. Also updated the CMakeLists.txt file to include the new header and its corresponding directory, ensuring that all aspects of the program can access the LCD features.

---
+++ include/LCDLibrary.h
@@ -0,0 +1,8 @@
+#ifndef LCDLIBRARY_LCDLIBRARY_H
+#define LCDLIBRARY_LCDLIBRARY_H
+
+#include "../LCDController.h"
+#include "../LCDGraphics.h"
+#include "../LCDDirectGraphics.h"
+
+#endif //LCDLIBRARY_LCDLIBRARY_H
